4a3c232e82cd397634f269f4a860c0f0b245aaf4
MhmmdAkcby <muhammedakcabey546@gmail.com>
2024-09-26 11:21:42 +0300

    firebase installation completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
@@ -6,7 +7,19 @@ import 'package:instagram_clone/responsive/web_screen_layout.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyAB-xwXuNiNcdHPd5-VIykTnKXwVQo7cpk",
+        appId: "1:941206901399:web:841ec187f782d811b64fd2",
+        messagingSenderId: "941206901399",
+        projectId: "instagram-tut-b9d62",
+        storageBucket: "instagram-tut-b9d62.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
